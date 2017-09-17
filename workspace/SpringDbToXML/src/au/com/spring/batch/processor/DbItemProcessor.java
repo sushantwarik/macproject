7efdbdcffc2058e6fb016e17f9e2bea632ebf6eb
Sushant Warik <sushant.warik@outlook.com>
2017-09-17 23:29:35 +1000

    HTTP Call added

--- workspace/SpringDbToXML/src/au/com/spring/batch/processor/DbItemProcessor.java
+++ workspace/SpringDbToXML/src/au/com/spring/batch/processor/DbItemProcessor.java
@@ -1,5 +1,15 @@
 package au.com.spring.batch.processor;
 
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.batch.item.ItemProcessor;
 
 import au.com.spring.batch.bo.Vehicle;
@@ -8,7 +18,24 @@ public class DbItemProcessor implements ItemProcessor<Vehicle,Vehicle> {
 
 	@Override
 	public Vehicle process(Vehicle vehicle) throws Exception {
-		System.out.println("Processing Vehicle " + vehicle.getModelNo());
+		try{
+			System.out.println("Processing Vehicle " + vehicle.getModelNo());
+			HttpClient provider = HttpClientBuilder.create().build();
+			HttpPost postMethod = new HttpPost("http://192.168.2.6:7801/xml");
+			JAXBContext contextObj = JAXBContext.newInstance(Vehicle.class);  
+			  
+		    Marshaller marshallerObj = contextObj.createMarshaller();  
+		    marshallerObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);  
+		    StringWriter sw = new StringWriter();
+		    marshallerObj.marshal(vehicle, sw);
+			postMethod.setEntity(new StringEntity(sw.toString()));
+			
+			HttpResponse response = provider.execute(postMethod);
+			System.out.println("HTTP Response status : " + response.getStatusLine());
+		} catch(Exception e){
+			e.printStackTrace(System.out);
+			System.out.println("Error : " + vehicle);
+		}
 		return vehicle;
 	}
 
