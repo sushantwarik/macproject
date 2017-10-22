07231eb51e14d3c9fd15b4393a4fd69424a03608
Sushant Warik <sushant.warik@outlook.com>
2017-10-22 13:58:30 +1100

    Bouncy castle change

--- workspace/SpringBatchSample/src/main/java/spring/security/StandardPBEEncDec.java
+++ workspace/SpringBatchSample/src/main/java/spring/security/StandardPBEEncDec.java
@@ -10,12 +10,17 @@ public class StandardPBEEncDec {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		StandardPBEStringEncryptor config = new StandardPBEStringEncryptor();
-		config.setProviderName("SunJCE");
-		config.setAlgorithm("AES");
+		
+		config.setProviderName("BC");
+		config.setAlgorithm("PBEWITHSHA256AND128BITAES-CBC-BC");
 		config.setPassword("Emmanuel");
 		String text = config.encrypt("This is the plaintext messsage");
 		System.out.println(text);
 		System.out.println(config.decrypt(text));
 		System.out.println(Cipher.getMaxAllowedKeyLength("AES"));
+		
+		//java.security file added security.provider.11=org.bouncycastle.jce.provider.BouncyCastleProvider
+		//bcprov-ext-jdk15on-158.jar copied to ../jre/lib/ext
+		
 	}
 }
